62d013dcf94469cccb90bcebf6cfe9014efc9be3
Marvin0555 <marcos.vinicius@oceanbrasil.com>
2023-01-08 15:04:23 -0400

    teste com qml

---
+++ QML_teste_de_telas/login_tela.py
@@ -0,0 +1,32 @@
+from PySide6.QtQml import QQmlApplicationEngine
+from PySide6.QtGui import QGuiApplication   
+from PySide6.QtCore import QObject, Slot
+from httpx import get
+from pathlib import Path
+
+class Ponte(QObject):
+    @Slot(str, result=list)
+    def fetch_image(self, pokemon_id):
+        image_path = Path('pokemon_image.png')
+        try:
+            if image_path.exists():
+                print('Deletando Imagem')
+                image_path.unlink()
+            response = get(f'https://pokeapi.co/api/v2/pokemon/{pokemon_id}').json()
+            image_url = response['sprites']['other']['home']['front_default']
+            image_content = get(image_url).content
+            with open('pokemon_image.png', 'wb') as poke_image:
+                poke_image.write(image_content)
+        except Exception as e:
+            print(e)
+        finally:
+            return str(image_path), response['name'].capitalize()
+
+app = QGuiApplication()
+engine = QQmlApplicationEngine()
+engine.load('teste_leta.qml')
+
+ponte = Ponte()
+context = engine.rootContext()
+context.setContextProperty('ponte', ponte)
+app.exec()
